7dd0ed2dcdd3c70e4cdf37f3b67ecd1d872a70da
ismailAmz <ismailalmuzamal1@gmail.com>
2024-05-17 02:21:32 +0700

    first commit

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: "https://www.dicoding.com/blog/registrasi-testing-themoviedb-api/",
+  BASE_URL: "https://api.themoviedb.org/3/",
+  BASE_IMAGE_URL: "https://image.tmdb.org/t/p/w500/",
+  DEFAULT_LANGUAGE: "en-us",
+};
+
+export default CONFIG;
